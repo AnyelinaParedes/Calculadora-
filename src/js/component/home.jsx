999995b3be80d3c5938a50fb248dc7f4eeda30cb
AnyelinaParedes <apg15194@hotmail.com>
2022-04-25 16:04:51 +0000

    calcularadora

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,61 @@
 import React from "react";
+import Boton from "./boton.jsx";
+import Pantalla from "./pantalla.jsx";
+import BotonClear from "./botonClear.jsx";
+import { useState } from "react";
+import { evaluate } from "mathjs"; //esta libreria sirve para operacion matematica para resultado
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/4geeks.png";
 
 //create your first component
-const Home = () => {
+function Home() {
+	const [input, setInput] = useState("");
+	const agregarInput = (valor) => {
+		setInput(input + valor);
+	};
+	const calcularResultado = () => {
+		if (input) {
+			setInput(evaluate(input));
+		} else {
+			alert("Por favor ingrese valores para realizar los cálculos.");
+		}
+	};
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App">
+			<div className="caculadora">
+				<Pantalla input={input} />
+				<div className="fila">
+					<Boton manejarClic={agregarInput}>1</Boton>
+					<Boton manejarClic={agregarInput}>2</Boton>
+					<Boton manejarClic={agregarInput}>3</Boton>
+					<Boton manejarClic={agregarInput}> +</Boton>
+				</div>
+				<div className="fila">
+					<Boton manejarClic={agregarInput}>4</Boton>
+					<Boton manejarClic={agregarInput}>5</Boton>
+					<Boton manejarClic={agregarInput}>6</Boton>
+					<Boton manejarClic={agregarInput}>-</Boton>
+				</div>
+				<div className="fila">
+					<Boton manejarClic={agregarInput}>7</Boton>
+					<Boton manejarClic={agregarInput}>8</Boton>
+					<Boton manejarClic={agregarInput}>9</Boton>
+					<Boton manejarClic={agregarInput}>*</Boton>
+				</div>
+				<div className="fila">
+					<Boton manejarClic={calcularResultado}>=</Boton>
+					<Boton manejarClic={agregarInput}>0</Boton>
+					<Boton manejarClic={agregarInput}>.</Boton>
+					<Boton manejarClic={agregarInput}>/</Boton>
+				</div>
+				<div className="fila">
+					<BotonClear manejarClear={() => setInput("")}>
+						Clear
+					</BotonClear>
+				</div>
+			</div>
 		</div>
 	);
-};
-
+}
 export default Home;
